1f0b1790afa7b17488d5b91c33f9916df9c61545
Zaprovic <jazaro2001@gmail.com>
2025-10-14 12:48:54 -0500

    style: implemented minmax with repeat to avoid media queries

--- src/app/(site)/shop/_components/product-grid/index.tsx
+++ src/app/(site)/shop/_components/product-grid/index.tsx
@@ -10,7 +10,10 @@ interface ProductGridProps {
 
 export function ProductGrid({ products }: ProductGridProps) {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div
+      className="grid gap-6"
+      style={{ gridTemplateColumns: "repeat(auto-fill, minmax(220px, 1fr))" }}
+    >
       {products.map((product) => (
         <Card
           key={product.id}
